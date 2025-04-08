99354fdc163028ddfa53e3e74632fadbe89c7d95
ugiv <ugiv@MacBook-Air.local>
2025-04-08 15:45:26 +0700

    Create google-auth authentication process

--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -3,6 +3,8 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import {pool} from '../database/Auth.database.js';
+import {google} from 'googleapis';
+
 
 dotenv.config();
 const router = express.Router();
@@ -80,6 +82,78 @@ router.get('/logout', (req, res) => {
     }
 }
 );
+
+
+// googleauth 
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "http://localhost:5001/auth/google/callback"
+);
+
+const scopes = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+];
+
+const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+    include_granted_scopes: true
+});
+
+
+router.get('/google', (req, res) => {
+    return res.redirect(url);
+});
+
+router.get('/google/callback', async (req, res) => {
+    const {code} = req.query;
+    const {tokens} = await oauth2Client.getToken(String(code));
+    oauth2Client.setCredentials(tokens);
+    const oauth2 = google.oauth2({
+        auth: oauth2Client,
+        version: 'v2'
+    });
+    oauth2.userinfo.get((err, response) => {
+        if (err){
+            console.log(err);
+        } else {
+            const {email, given_name} = response.data;
+            const password = '__';
+            const expireCookies = 4 * 60 * 60 * 1000;
+            try {
+                pool.query('SELECT email, id FROM user_data WHERE email = $1', [email], (err, results) => {
+                    console.log(results.rows);
+                    if (results.rows.length !== 0){
+                        const token = jwt.sign({email: email, id: results.rows[0].id}, 'secret');
+                        res.cookie("jwtToken", token, {
+                            httpOnly: true,
+                            maxAge: expireCookies,
+                            expire: Date.now() - expireCookies
+                        });
+                        res.status(200).redirect('http://localhost:3000/dashboard');
+                    } else {
+                        pool.query('INSERT INTO user_data(username, email, password) VALUES($1, $2, $3)', [given_name, email, password], (err, results) => {
+                            if (err){
+                                throw err;
+                            };
+                            const token = jwt.sign({email: email}, 'secret');
+                            res.cookie("jwtToken", token, {
+                                httpOnly: true,
+                                maxAge: expireCookies,
+                                expire: Date.now() - expireCookies
+                            });
+                            res.status(200).json({status: 'ok'});
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+});
 
 // router.get('/forgot-password', (req, res) => {
 //   res.send('Forgot Password Page');
